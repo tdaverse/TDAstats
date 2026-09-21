9514dc2fe49d4a0332786cfd070fbc5e84f7d7fe
Jason Cory Brunson <cornelioid@gmail.com>
2026-09-21 17:53:31 -0400

    align comments

--- R/visuals.R
+++ R/visuals.R
@@ -57,13 +57,13 @@ plot_persist <- function(feature.matrix, flat = FALSE,
                               y = c(0, axes.max))
     g <- ggplot2::ggplot(data = feature.df) +
       ggplot2::xlim(axes.min, axes.max) + ggplot2::ylim(axes.min, axes.max) +                           # axis limits
-      ggplot2::geom_abline(slope = 1, intercept = 0) +  # reference line
-      #ggplot2::geom_path(data = df.geompath, ggplot2::aes(x = .data[["x"]], y = .data[["y"]])) +          # reference segment
-      ggplot2::xlab("Feature appearance") + ggplot2::ylab("Feature disappearance") +                                 # axis titles
+      ggplot2::geom_abline(slope = 1, intercept = 0) +                                                  # reference line
+      #ggplot2::geom_path(data = df.geompath, ggplot2::aes(x = .data[["x"]], y = .data[["y"]])) +       # reference segment
+      ggplot2::xlab("Feature appearance") + ggplot2::ylab("Feature disappearance") +                    # axis titles
       ggplot2::theme(axis.line = ggplot2::element_line(colour = "black"),                               # add axis lines
-                     panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(),         # remove gridlines
-                     panel.background = ggplot2::element_blank()) +                                           # remove default background color
-     ggplot2::geom_point(ggplot2::aes(x = .data[["birth"]], y = .data[["death"]], shape = .data[["dimension"]], colour = .data[["dimension"]])) +   # add features as points
+                     panel.grid.major = ggplot2::element_blank(), panel.grid.minor = ggplot2::element_blank(), # remove gridlines
+                     panel.background = ggplot2::element_blank()) +                                     # remove default background color
+     ggplot2::geom_point(ggplot2::aes(x = .data[["birth"]], y = .data[["death"]], shape = .data[["dimension"]], colour = .data[["dimension"]])) + # add features as points
       ggplot2::coord_fixed(ratio = 1)
     
     # add line for bootstrap if desired
@@ -147,12 +147,12 @@ plot_barcode <- function(feature.matrix) {
   # plot w/ ggplot
   ggplot2::ggplot(data = feature.df) +
          ggplot2::xlim(x.min, x.max) + ggplot2::ylim(y.min, y.max) +                                                  # axis limits
-         ggplot2::xlab("Vietoris-Rips Diameter") + ggplot2::ylab("") +                                                  # axis titles
+         ggplot2::xlab("Vietoris-Rips Diameter") + ggplot2::ylab("") +                                                # axis titles
          ggplot2::theme(axis.line.x = ggplot2::element_line(colour = "black"),                                        # add x-axis line
                         axis.line.y = ggplot2::element_blank(), axis.ticks.y = ggplot2::element_blank(), axis.text.y = ggplot2::element_blank(), # remove y-axis stuff
                         panel.grid = ggplot2::element_blank(),                                                        # remove gridlines
                         panel.background = ggplot2::element_blank()) +                                                # remove default background color
-         ggplot2::geom_segment(ggplot2::aes(x = .data[["birth"]], y = .data[["vertical.pos"]],                                              # add actual bars for barcode
+         ggplot2::geom_segment(ggplot2::aes(x = .data[["birth"]], y = .data[["vertical.pos"]],                        # add actual bars for barcode
                           xend = .data[["death"]], yend = .data[["vertical.pos"]],
                           colour = .data[["dimension"]]))
 }
